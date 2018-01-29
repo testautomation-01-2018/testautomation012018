ee4f3e8c27dc7884a06da6e7f2bedd08d29d6b6f
testautomation-01-2018 <automation012018@wp.pl>
2018-01-29 15:16:04 +0100

    Commit junit kolejny i jeszcze jeden

--- src/test/java/jUnitTest/FirstJUnitTest.java
+++ src/test/java/jUnitTest/FirstJUnitTest.java
@@ -4,9 +4,11 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.endsWith;
-import static org.hamcrest.CoreMatchers.is;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.*;
 
 public class FirstJUnitTest extends ConfigJUnit {
@@ -40,7 +42,7 @@ public class FirstJUnitTest extends ConfigJUnit {
         assertThat("to jest test na that", testowy, is("testowy"));
         assertTrue(naszValue == 15);
         assertThat(testowy2, containsString("testowy"));
-        assertThat(testowy2, endsWith("y"));
+        assertThat(testowy2.toLowerCase(), endsWith("y"));
     }
 
     @Test
@@ -48,5 +50,15 @@ public class FirstJUnitTest extends ConfigJUnit {
         Integer wartoscTestowa = null;
         assertNull(wartoscTestowa);
         assertNull(wartoscTestowa);
+    }
+
+    List<Integer> integery = Arrays.asList(1,2,3,4,5,6);
+    List<Integer> integery2 = Arrays.asList(1,2,3,4,5,6);
+
+    @Test
+    public void trzeciTest() {
+        assertThat(integery, hasItem(5));
+        assertThat(integery, hasSize(6));
+        assertArrayEquals(integery.toArray(), integery2.toArray());
     }
 }
