3edf4a249adcf2d1715c3ced5a7163f21efa36fb
testautomation-01-2018 <automation012018@wp.pl>
2018-01-31 08:25:43 +0100

    Commit jupiter

--- src/test/java/jUnitTest/JUpiterTest.java
+++ src/test/java/jUnitTest/JUpiterTest.java
@@ -1,6 +1,7 @@
 package jUnitTest;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
@@ -9,16 +10,39 @@ import static junit.framework.TestCase.assertTrue;
 @DisplayName("===== sdfsdfsdfsdfsdfsdf =======")
 public class JUpiterTest extends ConfigJupiter {
 
-    int testowy = 20;
-    String testowyString = "Ala ma kota";
 
-    @Test
-    @DisplayName("== to jest test jupitera =====")
-    @RepeatedTest(5)
-    public void jupiterTest() {
+    @Nested
+    @DisplayName("=========== jupiter nested klas=====")
+    public class JupiterParameterized {
+
+
+        int testowy = 20;
+        String testowyString = "Ala ma kota";
+
+        @Test
+        @DisplayName("== to jest test jupitera =====")
+        @RepeatedTest(5)
+        public void jupiterTest () {
         System.out.println("jupiter");
         assertTrue(testowy == 20);
     }
 
+    }
 
+    @Nested
+    @DisplayName("=========== jupiter nested next klas=====")
+    public class JupiterParameterizedTest {
+
+
+        int testowy = 20;
+        String testowyString = "Ala ma kota";
+
+        @Test
+        @DisplayName("== to jest test jupitera =====")
+        @RepeatedTest(5)
+        public void jupiterNextTest () {
+            System.out.println("jupiter");
+            assertTrue(testowy == 20);
+        }
+    }
 }
