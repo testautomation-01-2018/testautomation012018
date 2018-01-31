a1ffc6f89374eebfb725f9a40f9c5197dc9351ba
testautomation-01-2018 <automation012018@wp.pl>
2018-01-31 09:09:30 +0100

    Commit jupiter

--- src/test/java/jUnitTest/JUpiterTest.java
+++ src/test/java/jUnitTest/JUpiterTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static junit.framework.TestCase.assertTrue;
 
@@ -11,38 +14,67 @@ import static junit.framework.TestCase.assertTrue;
 public class JUpiterTest extends ConfigJupiter {
 
 
-    @Nested
-    @DisplayName("=========== jupiter nested klas=====")
-    public class JupiterParameterized {
+        @Nested
+        @DisplayName("=========== jupiter nested klas=====")
+        public class JupiterParameterized {
 
 
-        int testowy = 20;
-        String testowyString = "Ala ma kota";
+            int testowy = 20;
+            String testowyString = "Ala ma kota";
 
-        @Test
-        @DisplayName("== to jest test jupitera =====")
-        @RepeatedTest(5)
-        public void jupiterTest () {
-        System.out.println("jupiter");
-        assertTrue(testowy == 20);
-    }
+            @Test
+            @DisplayName("== to jest test jupitera =====")
+            @RepeatedTest(5)
+            public void jupiterTest() {
+                System.out.println("jupiter");
+                assertTrue(testowy == 20);
+            }
 
-    }
-
-    @Nested
-    @DisplayName("=========== jupiter nested next klas=====")
-    public class JupiterParameterizedTest {
-
-
-        int testowy = 20;
-        String testowyString = "Ala ma kota";
-
-        @Test
-        @DisplayName("== to jest test jupitera =====")
-        @RepeatedTest(5)
-        public void jupiterNextTest () {
-            System.out.println("jupiter");
-            assertTrue(testowy == 20);
         }
-    }
+
+        @Nested
+        @DisplayName("=========== jupiter nested next klas=====")
+        public class JupiterParameterizedTest {
+
+
+            int testowy = 20;
+            String testowyString = "Ala ma kota";
+
+            @Test
+            @DisplayName("== to jest test jupitera =====")
+            @RepeatedTest(5)
+            public void jupiterNextTest() {
+                System.out.println("jupiter");
+                assertTrue(testowy == 20);
+            }
+        }
+
+        @Nested
+        @DisplayName("===== jupiter nested text ")
+        public class nextNestedTest{
+
+            @Test
+            @DisplayName("== to jest test jupitera =====")
+            @RepeatedTest(2)
+            public void jupiterNext2Test() {
+                System.out.println("jupiter");
+                assertTrue(5*4 == 20);
+            }
+
+            @ParameterizedTest
+            @DisplayName("Test z CSV")
+            @CsvFileSource(resources = "/plik.csv")
+            public void csvTest(String name, int age){
+                assertTrue(name instanceof String);
+//                assertTrue(age instanceof Integer);
+                assertTrue(age < 1000);
+            }
+
+            @ParameterizedTest
+            @ValueSource(ints = {1,2,3,4})
+            public void withValueSourceTest(int value){
+                assertTrue(value < 100);
+            }
+
+        }
 }
